bce45fc58f0c734c1c29010db1c0ae4ca864d23c
Husnain Ali Official <141839505+HusnainAliOfficial@users.noreply.github.com>
2023-10-09 08:02:12 +0500

    Update App.py

--- Chat bot Using python data relative to banking system/App.py
+++ Chat bot Using python data relative to banking system/App.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import subprocess
+
+
+st.title("Tic Tac TOe AI ")
+
+user_input = st.text_input("Type 'bye' to close the app:")
+
+if user_input.lower() == 'bye':
+    st.write("Closing the app...")
+    subprocess.Popen(["pkill", "streamlit"])
+if st.button("Clear Content"):
+    st.write("")
+if st.button("Generate"):
+    generated_text = cb.chat_with_user(user_input)
+    st.write("Generated Text:", generated_text)
+
 
